76ea0e0007933990adf4f419b1319d64dbb225d1
braionn <thebraionn@gmail.com>
2018-04-30 18:15:55 +0200

    One more class added

---
+++ src/Primer5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modul1Termin05.Primer5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jos neka klasa");
+            Console.ReadKey(false);
+        }
+    }
+}
