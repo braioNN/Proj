dfd4de302624c8074b084aa504ed84221d0bcf24
braionn <thebraionn@gmail.com>
2018-05-03 18:29:55 +0200

    refresh

--- src/Primer1/MainClass1.cs
+++ src/Primer1/MainClass1.cs
@@ -15,7 +15,7 @@ namespace Modul1Termin05.Primer1
             int[] niz = new int[] {1,2,3};
             try
             {
-                //1
+                //1 
                 //broj = Int32.Parse("tri");
 
                 //2
